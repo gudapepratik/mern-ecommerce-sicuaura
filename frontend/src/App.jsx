8b3dc039349af27a968c6ba8f0f7dfbba30a4d2a
Pratik <pratikgudape9825@gmail.com>
2025-05-29 10:38:45 +0530

    frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='w-full text-red-400 flex h-20 items-center bg-red-100 '>Hello there</h1>
+    </>
+  )
+}
+
+export default App
